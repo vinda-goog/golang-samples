12260047419aa117cd58a383b04c76eb895ca8f2
Arvind Srinivasan <vinda@google.com>
2020-05-01 01:22:22 -0400

    revised commit of service directory samples

--- servicedirectory/endpoints_test.go
+++ servicedirectory/endpoints_test.go
@@ -1,3 +1,17 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package servicedirectory
 
 import (
@@ -36,7 +50,7 @@ func TestEndpoint(t *testing.T) {
 
 		got := buf.String()
 		fmt.Println(got)
-		if want := "services/golang-test-endpoint"; !strings.Contains(got, want) {
+		if want := "endpoints/golang-test-endpoint"; !strings.Contains(got, want) {
 			t.Errorf("got %q, want %q", got, want)
 		}
 	})
@@ -53,7 +67,7 @@ func TestEndpoint(t *testing.T) {
 	deleteErr := deleteNamespace(tc.ProjectID)
 
 	if deleteErr != nil {
-		fmt.Println("Failed to delete namespace in test tear down.")
+		fmt.Printf("Failed to delete namespace in test tear down: %v.", deleteErr)
 	}
 }
 
